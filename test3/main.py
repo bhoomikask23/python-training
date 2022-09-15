5daad9c0e20d3b5871aa35a188bdebd5123aab64
bhoomikask23 <113244357+bhoomikask23@users.noreply.github.com>
2022-09-15 15:27:31 +0530

    Add files via upload

---
+++ test3/main.py
@@ -0,0 +1,19 @@
+
+
+
+import cal
+import sys
+firstname= input("enter the firstname : ")
+lastname = input("enter the lastname : ")
+
+
+def salary():
+    
+    print('hra is:' ,cal.hra())
+    print('da :',cal.da())
+    print("bonus :",cal.bonus())
+salary()
+
+
+
+
